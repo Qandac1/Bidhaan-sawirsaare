6448ee3a887b84e003f9a81d04b61860c01a6dbf
blandk77 <alexags1983@gmail.com>
2025-04-06 15:10:41 +0530

    Create script.py

---
+++ script.py
@@ -0,0 +1,151 @@
+from config import Config
+
+class rkn(object):
+    # part of text configuration
+    START_TXT = f"""<b>Ｈ𝙸, {}👋
+
+𝚃ʜɪs 𝙸s 𝙰ɴ 𝙰ᴅᴠᴀᴄᴇᴅ 𝙰ɴᴅ 𝚈ᴇᴛ 𝙿ᴏᴡᴇʀғᴜʟ 𝚁ᴇɴᴀᴍᴇ 𝙱ᴏᴛ
+𝚄sɪɴɢ 𝚃ʜɪs 𝙱ᴏᴛ 𝚈ᴏᴜ 𝙲ᴀɴ 𝚁ᴇɴᴀᴍᴇ & 𝙲ʜᴀɴɢᴇ 𝚃ʜᴜᴍʙɴᴀɪʟ 𝙾ғ 𝚈ᴏᴜʀ 𝙵ɪʟᴇ 
+𝚈ᴏᴜ 𝙲ᴀɴ 𝙰ʟsᴏ 𝙲ᴏɴᴠᴇʀᴛ 𝚅ɪᴅᴇᴏ 𝚃ᴏ 𝙵ɪʟᴇ & 𝙵ɪʟᴇ 𝚃ᴏ 𝚅ɪᴅᴇᴏ
+𝚃𝙷𝙸𝚂 𝙱𝙾𝚃 𝙰𝙻𝚂𝙾 𝚂𝚄𝙿𝙿𝙾𝚁𝚃𝚂 𝙲𝚄𝚂𝚃𝙾𝙼 𝚃𝙷𝚄𝙼𝙱𝙽𝙰𝙸𝙻 𝙰𝙽𝙳 𝙲𝚄𝚂𝚃𝙾𝙼 𝙲𝙰𝙿𝚃𝙸𝙾𝙽
+
+Tʜɪs Bᴏᴛ Wᴀs Cʀᴇᴀᴛᴇᴅ Bʏ : @{Config.UPDATES} 💞</b>"""
+
+    ABOUT_TXT = """<b>╭───────────⍟
+├🤖 ᴍy ɴᴀᴍᴇ : {}
+├🖥️ Dᴇᴠᴇʟᴏᴩᴇʀꜱ : {}
+├👨‍💻 Pʀᴏɢʀᴀᴍᴇʀ : {}
+├📕 Lɪʙʀᴀʀy : {}
+├✏️ Lᴀɴɢᴜᴀɢᴇ: {}
+├💾 Dᴀᴛᴀ Bᴀꜱᴇ: {}</b>     
+╰───────────────⍟ """
+
+    HELP_TXT = """
+<b>•></b> /start Tʜᴇ Bᴏᴛ.
+
+✏️ <b><u>Hᴏᴡ Tᴏ Rᴇɴᴀᴍᴇ A Fɪʟᴇ</u></b>
+<b>•></b> Sᴇɴᴅ Aɴy Fɪʟᴇ Aɴᴅ Tyᴩᴇ Nᴇᴡ Fɪʟᴇ Nɴᴀᴍᴇ \nAɴᴅ Aᴇʟᴇᴄᴛ Tʜᴇ Fᴏʀᴍᴀᴛ [ document, video, audio ].
+"""
+
+    UPGRADE_PREMIUM= """
+•⪼ ★𝘗𝘭𝘢𝘯𝘴    -  ⏳𝘋𝘢𝘵𝘦 - 💸𝘗𝘳𝘪𝘤𝘦 
+•⪼ 🥉𝘉𝘳𝘰𝘯𝘻𝘦  -   3𝘥𝘢𝘺𝘴 -   39
+•⪼ 🥈𝘚𝘪𝘭𝘷𝘦𝘳   -   7𝘥𝘢𝘺𝘴 -   59
+•⪼ 🥇𝘎𝘰𝘭𝘥    -  15𝘥𝘢𝘺𝘴 -  99
+•⪼ 🏆𝘗𝘭𝘢𝘵𝘪𝘯𝘶𝘮 -  1𝘮𝘰𝘯𝘵𝘩 -  179
+•⪼ 💎𝘋𝘪𝘢𝘮𝘰𝘯𝘥 -  2𝘮𝘰𝘯𝘵𝘩 -  339
+
+- 𝘋𝘢𝘪𝘭𝘺 𝘜𝘱𝘭𝘰𝘢𝘥 𝘓𝘪𝘮𝘪𝘵 𝘜𝘯𝘭𝘪𝘮𝘪𝘵𝘦𝘥
+    """
+    
+    UPGRADE_PLAN= """
+𝘗𝘭𝘢𝘯: 𝘗𝘳𝘰
+𝘋𝘢𝘵𝘦: 1 𝘮𝘰𝘯𝘵𝘩 
+𝘗𝘳𝘪𝘤𝘦: 179
+𝘓𝘪𝘮𝘪𝘵: 100 𝘎𝘉
+
+𝘗𝘭𝘢𝘯: 𝘜𝘭𝘵𝘢 𝘗𝘳𝘰 
+𝘋𝘢𝘵𝘦: 1 𝘮𝘰𝘯𝘵𝘩 
+𝘗𝘳𝘪𝘤𝘦: 199
+𝘓𝘪𝘮𝘪𝘵: 1000 𝘎𝘉
+
+- 𝘋𝘪𝘴𝘤𝘰𝘶𝘯𝘵 𝘈𝘭𝘭 𝘗𝘭𝘢𝘯 𝘙𝘴.9
+    """
+    
+    THUMBNAIL = """
+🌌 <b><u>Hᴏᴡ Tᴏ Sᴇᴛ Tʜᴜᴍʙɴɪʟᴇ</u></b>
+
+<b>•></b> Sᴇɴᴅ Aɴy Pʜᴏᴛᴏ Tᴏ Aᴜᴛᴏᴍᴀᴛɪᴄᴀʟʟy Sᴇᴛ Tʜᴜᴍʙɴɪʟᴇ.
+<b>•></b> /del_thumb Uꜱᴇ Tʜɪꜱ Cᴏᴍᴍᴀɴᴅ Tᴏ Dᴇʟᴇᴛᴇ Yᴏᴜʀ Oʟᴅ Tʜᴜᴍʙɴɪʟᴇ.
+<b>•></b> /view_thumb Uꜱᴇ Tʜɪꜱ Cᴏᴍᴍᴀɴᴅ Tᴏ Vɪᴇᴡ Yᴏᴜʀ Cᴜʀʀᴇɴᴛ Tʜᴜᴍʙɴɪʟᴇ.
+"""
+    CAPTION= """
+📑 <b><u>Hᴏᴡ Tᴏ Sᴇᴛ Cᴜꜱᴛᴏᴍ Cᴀᴩᴛɪᴏɴ</u></b>
+
+<b>•></b> /set_caption - Uꜱᴇ Tʜɪꜱ Cᴏᴍᴍᴀɴᴅ Tᴏ Sᴇᴛ ᴀ Cᴜꜱᴛᴏᴍ Cᴀᴩᴛɪᴏɴ
+<b>•></b> /see_caption - Uꜱᴇ Tʜɪꜱ Cᴏᴍᴍᴀɴᴅ Tᴏ Vɪᴇᴡ Yᴏᴜʀ Cᴜꜱᴛᴏᴍ Cᴀᴩᴛɪᴏɴ
+<b>•></b> /del_caption - Uꜱᴇ Tʜɪꜱ Cᴏᴍᴍᴀɴᴅ Tᴏ Dᴇʟᴇᴛᴇ Yᴏᴜʀ Cᴜꜱᴛᴏᴍ Cᴀᴩᴛɪᴏɴ
+
+Exᴀᴍᴩʟᴇ:- `/set_caption 📕 Fɪʟᴇ Nᴀᴍᴇ: {filename}
+💾 Sɪᴢᴇ: {filesize}
+⏰ Dᴜʀᴀᴛɪᴏɴ: {duration}`
+"""
+    BOT_STATUS = """
+⚡️ ʙᴏᴛ sᴛᴀᴛᴜs ⚡️
+
+⌚️ ʙᴏᴛ ᴜᴩᴛɪᴍᴇ: `{}`
+👭 ᴛᴏᴛᴀʟ ᴜsᴇʀꜱ: `{}`
+💸 ᴛᴏᴛᴀʟ ᴘʀᴇᴍɪᴜᴍ ᴜsᴇʀs: `{}`
+֍ ᴜᴘʟᴏᴀᴅ: `{}`
+⊙ ᴅᴏᴡɴʟᴏᴀᴅ: `{}`
+"""
+    LIVE_STATUS = """
+⚡ ʟɪᴠᴇ sᴇʀᴠᴇʀ sᴛᴀᴛᴜs ⚡
+
+ᴜᴘᴛɪᴍᴇ: `{}`
+ᴄᴘᴜ: `{}%`
+ʀᴀᴍ: `{}%` 
+ᴛᴏᴛᴀʟ ᴅɪsᴋ: `{}`
+ᴜsᴇᴅ sᴘᴀᴄᴇ: `{} {}%`
+ғʀᴇᴇ sᴘᴀᴄᴇ: `{}`
+ᴜᴘʟᴏᴀᴅ: `{}`
+ᴅᴏᴡɴʟᴏᴀᴅ: `{}`
+V𝟹.𝟶.𝟶 [STABLE]
+"""
+    DIGITAL_METADATA = f"""
+❪ SET CUSTOM METADATA ❫
+
+- /metadata - Tᴏ Sᴇᴛ & Cʜᴀɴɢᴇ ʏᴏᴜʀ ᴍᴇᴛᴀᴅᴀᴛᴀ ᴄᴏᴅᴇ
+
+☞ Fᴏʀ Exᴀᴍᴘʟᴇ:-
+
+`--change-title @{Config.UPDATES}
+--change-video-title @{Config.UPDATES}
+--change-audio-title @{Config.UPDATES}
+--change-subtitle-title @{Config.UPDATES}
+--change-author @{Config.UPDATES}`
+
+📥 Fᴏʀ Hᴇʟᴘ Cᴏɴᴛ. @{Config.UPDATES}
+"""
+    
+    CUSTOM_FILE_NAME = f"""
+<u>🖋️ Custom File Name</u>
+
+you can pre-add a prefix and suffix along with your new filename
+
+➢ /set_prefix - To add a prefix along with your _filename.
+➢ /see_prefix - Tᴏ Sᴇᴇ Yᴏᴜʀ Pʀᴇғɪx !!
+➢ /del_prefix - Tᴏ Dᴇʟᴇᴛᴇ Yᴏᴜʀ Pʀᴇғɪx !!
+➢ /set_suffix - To add a suffix along with your filename_.
+➢ /see_suffix - Tᴏ Sᴇᴇ Yᴏᴜʀ Sᴜғғɪx !!
+➢ /del_suffix - Tᴏ Dᴇʟᴇᴛᴇ Yᴏᴜʀ Sᴜғғɪx !!
+
+Exᴀᴍᴩʟᴇ:- `/set_suffix @{Config.UPDATES}`
+Exᴀᴍᴩʟᴇ:- `/set_prefix @{Config.UPDATES}`
+"""
+    
+   
+    DEV_TXT = f"""Bot hosted by @{Config.UPDATES}"""
+    
+
+    SEND_METADATA = f"""
+❪ SET CUSTOM METADATA ❫
+
+☞ Fᴏʀ Exᴀᴍᴘʟᴇ:-
+
+`--change-title @{Config.UPDATES}
+--change-video-title @{Config.UPDATES}
+--change-audio-title @{Config.UPDATES}
+--change-subtitle-title @{Config.UPDATES}
+--change-author @{Config.UPDATES}`
+
+just replace `@{Config.UPDATES}` with your Metadata Text
+"""
+    
+    RKN_PROGRESS = """<b>\n
+╭━━━━❰✨..PROCESSING..✨❱━➣
+┣⪼ 🗃️ ꜱɪᴢᴇ: {1} | {2}
+┣⪼ ⏳️ ᴅᴏɴᴇ : {0}%
+┣⪼ 🚀 ꜱᴩᴇᴇᴅ: {3}/s
+┣⪼ ⏰️ ᴇᴛᴀ: {4}
+╰━━━━━━━━━━━━━━━➣ </b>"""
